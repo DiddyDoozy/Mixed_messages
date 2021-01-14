3a2e8a37222742382e4f9ee2ce46c5ae1b90f82f
DiddyDoozy <terryellis97@gmail.com>
2021-01-14 13:53:54 -0500

    modified sentence structure and capitalized first letter

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const verbs = ['run', 'walk', 'drive', 'dance', 'scream', 'joust', 'flip', 'lift'];
+const verbs = ['run', 'walk', 'drive', 'dance', 'scream', 'joust', 'flip', 'lift','scissor'];
+const identifiers = ['this', 'that','my','your','the'];
 
-const nouns = ['that cat','this moose','this hoe','Lando','my girlfriend','my johnson'];
+let randNums = [0,0,0,0,0,0,0,0,0,0];
 
-const adjectives = ['hard','soft','weird','strange','cute','pretty','fat','dumb','thicc'];
+const nouns = ['cat','moose','hoe','Lando','girlfriend','johnson'];
+
+const adjectives = ['hard','soft','weird','strange','cute','nasty','pretty','fat','dumb','thicc'];
 // "noun wants to "verb" "adjective" on "noun""
 
+let randSentence = '';
+
+function makeRand(arr){
+    return Math.floor(Math.random()*arr.length);
+};
+
+
 const makeRandomSentence = () => {
-    let randVerb = Math.floor(Math.random()*verbs.length);
-    let randNoun2 = Math.floor(Math.random()*nouns.length);
-    let randNoun = Math.floor(Math.random()*nouns.length);
-    let randAdj = Math.floor(Math.random()*adjectives.length);
-    return `${nouns[randNoun]} wants to ${verbs[randVerb]} ${adjectives[randAdj]} on ${nouns[randNoun2]}.`
+    let randId = makeRand(identifiers);
+    let randId2 = makeRand(identifiers);
+    let randVerb = makeRand(verbs);
+    let randNoun2 = makeRand(nouns);
+    let randNoun = makeRand(nouns);
+    let randAdj = makeRand(adjectives);
+    let randAdj2 = makeRand(adjectives);
+    randSentence = `${identifiers[randId]} ${adjectives[randAdj]} ${nouns[randNoun]} wants to ${verbs[randVerb]} ${adjectives[randAdj2]} on ${identifiers[randId2]} ${nouns[randNoun2]}.`;
+    let newSen = randSentence.charAt(0).toUpperCase() + randSentence.slice(1);
+    return newSen;
 };
 console.log(makeRandomSentence());
